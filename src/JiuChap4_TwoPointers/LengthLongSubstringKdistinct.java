a886c0a994ef798c005f059068c70e38b885f120
tzhangBlast <tzhang@blastmotion.com>
2015-11-10 10:10:10 -0800

    Substring+Map+maintain constraint+1337+Ruibi

---
+++ src/JiuChap4_TwoPointers/LengthLongSubstringKdistinct.java
@@ -0,0 +1,77 @@
+package JiuChap4_TwoPointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * http://www.lintcode.com/en/problem/longest-substring-with-at-most-k-distinct-characters/
+ * Created this class in JiuChap4_TwoPointers at 9:33 AM, 11/10/2015.
+ */
+public class LengthLongSubstringKdistinct {
+  public static void main(String[] args) {
+    String s = "eqgkcwGFvjjmxutystqdfhuMblWbylgjxsxgnoh"; // "eceba";
+    int k = 16; // 3;
+    LengthLongSubstringKdistinct lsk = new LengthLongSubstringKdistinct();
+    int ans = lsk.lengthOfLongestSubstringKDistinct(s, k);
+    System.out.println(ans);
+  }
+
+  /**
+   * http://blog.csdn.net/whuwangyi/article/details/42451289
+   * Same idea as Rui Bi's minimum size substring contains target string.
+   */
+  public int lengthOfLongestSubstringKDistinct(String s, int k) {
+    // write your code here
+    int ans = 0;
+    int i = 0;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int j = 0; j < s.length(); ++j) {
+      char ch = s.charAt(j);
+      if (map.containsKey(ch)) {
+        map.put(ch, map.get(ch) + 1);
+      }
+      else {
+        map.put(ch, 1);
+        while (map.size() > k) {
+          char preChar = s.charAt(i++);
+          if (map.get(preChar)>1) {
+            map.put(preChar, map.get(preChar) - 1);
+          }
+          else {
+            map.remove(preChar);
+          }
+        }
+      }
+      ans = Math.max(ans, j - i + 1);
+    }
+    return ans;
+  }
+
+
+  /**
+   * @param s : A string
+   * @return : The length of the longest substring
+   *           that contains at most k distinct characters.
+   */
+  public int lengthOfLongestSubstringKDistinctWRONG(String s, int k) {
+    // write your code here
+    int ans = 0;
+    int j = 0;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < s.length(); ++i) {
+      while (j < s.length()) {
+        if (map.size() <= k) {
+          map.put(s.charAt(j), map.containsKey(s.charAt(j)) ? map.get(s.charAt(j)) + 1 : 1);
+          ans = Math.max(ans, j-i);
+          j++;
+          continue;
+        }
+        else {
+          map.put(s.charAt(i), map.containsKey(s.charAt(i)) ? map.get(s.charAt(i)) - 1 : 0);
+          break;
+        }
+      }
+    }
+    return ans;
+  }
+}
